eac327e1af023ff916c8466a7f7ec25068e7c374
sd.lee <sd.lee@ninetap.com>
2022-08-31 11:10:22 +0900

    [이상동] 서브 모듈 갱신

--- Scripts/Runtime/Manager/CGameCenterManager.cs
+++ Scripts/Runtime/Manager/CGameCenterManager.cs
@@ -194,6 +194,15 @@ public partial class CGameCenterManager : CSingleton<CGameCenterManager> {
 	}
 	#endregion			// 함수
 
+	#region 클래스 함수
+	/** 매개 변수를 생성한다 */
+	public static STParams MakeParams(Dictionary<ECallback, System.Action<CGameCenterManager, bool>> a_oCallbackDict) {
+		return new STParams() {
+			m_oCallbackDict = a_oCallbackDict
+		};
+	}
+	#endregion			// 클래스 함수
+
 	#region 조건부 함수
 #if UNITY_IOS || UNITY_ANDROID
 	/** 초기화 되었을 경우 */
